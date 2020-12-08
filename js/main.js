a9f4ccc77e536a1114aa4dcac3dfa402b5be1496
JamesHow1ett <mopis101@gmail.com>
2020-12-08 21:36:38 +0200

    tablet: layout promo and category blocks
    Signed-off-by: JamesHow1ett <mopis101@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (window.innerWidth < 768 || window.outerWidth < 768) {
     ininMobileSliders();
   } else if ((window.innerWidth >= 768 || window.outerWidth >= 768)
-    && window.innerWidth >= 768 || window.outerWidth >= 768) {
+    && (window.innerWidth < 1400 || window.outerWidth < 1400)) {
       destroyMobileSliders();
       initTabletSliders();
   }
@@ -71,10 +71,10 @@ function ininMobileSliders () {
 }
 
 function destroyMobileSliders () {
-  $('.main-slider').on('destroy', function(event, slick){});
-  $('.promo-slider').on('destroy', function(event, slick){});
-  $('.instagram-slider').on('destroy', function(event, slick){});
-  $('.quem-usa-slider').on('destroy', function(event, slick){});
+  $('.main-slider').on('destroy', (event, slick) => {});
+  $('.promo-slider').on('destroy', (event, slick) => {});
+  $('.instagram-slider').on('destroy', (event, slick) => {});
+  $('.quem-usa-slider').on('destroy', (event, slick) => {});
 }
 
 function initTabletSliders () {
@@ -87,6 +87,19 @@ function initTabletSliders () {
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: []
+  });
+  $('.promo-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<button type="button" class="slick-prev arrow arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next arrow arrow-next"></button>',
     responsive: []
   });
 }
