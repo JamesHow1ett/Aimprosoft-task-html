dfad0f66d5539cf0ea6a024de96d4bf0e1dd3468
JamesHow1ett <mopis101@gmail.com>
2020-12-10 23:13:44 +0200

    desctop: layout all blocks, added modal window for menu
    Signed-off-by: JamesHow1ett <mopis101@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,14 +1,65 @@
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.innerWidth < 768 || window.outerWidth < 768) {
-    ininMobileSliders();
-  } else if ((window.innerWidth >= 768 || window.outerWidth >= 768)
-    && (window.innerWidth < 1200 || window.outerWidth < 1200)) {
-      destroyMobileSliders();
-      initTabletSliders();
-  } else {
-    initDesctopSliders();
-  }
-});
+const menuBtn = document.getElementById('menu-btn');
+const navBar = document.querySelector('.menu');
+const modalWindowMenu = document.querySelector('.modal-window-menu');
+
+
+const menuDivModal = document.createElement('div');
+const modalMenu = `<div class="open-menu">
+                    <div class="open-menu__title">
+                      <div class="open-menu__title-text">Menu</div>
+                      <button id="menu-close" class="open-menu__title-btn-close">
+                        <i class="fas fa-times fa-2x"></i>
+                      </button>
+                    </div>
+                    <div class="open-menu__search">
+                      <div class="open-menu__search-input">
+                        <input type="text" placeholder="Search">
+                      </div>
+                      <button class="open-menu__search-btn">
+                        <i class="fas fa-search"></i>
+                      </button>
+                    </div>
+                    <div class="open-menu__menu-item sobre">Sobre
+                      <ul class="open-menu__list">
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                      </ul>
+                    </div>
+                    <div class="open-menu__menu-item cols">Colecoes
+                      <ul class="open-menu__list">
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                      </ul>
+                    </div>
+                    <div class="open-menu__menu-item catg">Category
+                      <ul class="open-menu__list">
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                      </ul>
+                    </div>
+                    <div class="open-menu__menu-item looks">Looks
+                      <ul class="open-menu__list">
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                      </ul>
+                    </div>
+                    <div class="open-menu__menu-item contato">Contato
+                      <ul class="open-menu__list">
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                        <li class="open-menu__item">Item</li>
+                      </ul>
+                    </div>
+                  </div>`
 
 function ininMobileSliders () {
   $('.main-slider').slick({
@@ -135,3 +186,83 @@ function initDesctopSliders () {
     responsive: []
   });
 }
+
+const openMenu = () => {
+  modalWindowMenu.style.display = 'block';
+  menuDivModal.innerHTML = modalMenu
+  navBar.append(menuDivModal);
+
+  const openMenuModal = document.querySelector('.open-menu');
+  openMenuModal.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.id === 'menu-close' || target.parentNode.id === 'menu-close') {
+      modalWindowMenu.style.display = 'none';
+      openMenuModal.style.display = 'none';
+    }
+
+    if (target.classList[0] === 'open-menu__menu-item') {
+      switch (target.classList[1]) {
+        case 'sobre':
+          if (target.firstElementChild.classList.contains('show')) {
+            target.firstElementChild.classList.remove('show');
+          } else {
+            target.firstElementChild.classList.add('show');
+            target.style.paddingBottom = '0';
+          }
+          break;
+        case 'cols':
+          if (target.firstElementChild.classList.contains('show')) {
+            target.firstElementChild.classList.remove('show');
+          } else {
+            target.firstElementChild.classList.add('show');
+            target.style.paddingBottom = '0';
+          }
+          break;
+        case 'catg':
+          if (target.firstElementChild.classList.contains('show')) {
+            target.firstElementChild.classList.remove('show');
+          } else {
+            target.firstElementChild.classList.add('show');
+            target.style.paddingBottom = '0';
+          }
+          break;
+        case 'looks':
+          if (target.firstElementChild.classList.contains('show')) {
+            target.firstElementChild.classList.remove('show');
+          } else {
+            target.firstElementChild.classList.add('show');
+            target.style.paddingBottom = '0';
+          }
+          break;
+        case 'contato':
+          if (target.firstElementChild.classList.contains('show')) {
+            target.firstElementChild.classList.remove('show');
+          } else {
+            target.firstElementChild.classList.add('show');
+            target.style.paddingBottom = '0';
+          }
+          break;
+        default:
+          return;
+      }
+    }
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 768 || window.outerWidth < 768) {
+    ininMobileSliders();
+  } else if ((window.innerWidth >= 768 || window.outerWidth >= 768)
+    && (window.innerWidth < 1200 || window.outerWidth < 1200)) {
+      destroyMobileSliders();
+      initTabletSliders();
+  } else {
+    initDesctopSliders();
+  }
+
+
+  menuBtn.addEventListener('click', openMenu);
+
+});
